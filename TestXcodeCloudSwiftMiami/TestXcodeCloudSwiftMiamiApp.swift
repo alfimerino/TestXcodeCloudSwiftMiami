94899259608b45b4e323df0d2e5ba3ded8471a29
Alfredo Merino <alfimerino@gmail.com>
2024-02-29 19:49:08 -0500

    Initial Commit

---
+++ TestXcodeCloudSwiftMiami/TestXcodeCloudSwiftMiamiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestXcodeCloudSwiftMiamiApp.swift
+//  TestXcodeCloudSwiftMiami
+//
+//  Created by Alfredo Merino on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TestXcodeCloudSwiftMiamiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
